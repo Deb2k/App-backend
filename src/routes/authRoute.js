f89c29fba3cba800d7a17b299a38a3f3aceef419
Debashis Phukan <73440520+Deb2k@users.noreply.github.com>
2025-08-25 21:48:45 +0530

    initial commit

---
+++ src/routes/authRoute.js
@@ -0,0 +1,59 @@
+import express from 'express'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import db from '../db.js'
+
+const router = express.Router()
+
+//Register a new user
+router.post('/register', async(req ,res)=>{
+    const {username, password} = req.body;
+    const hashedPassword = bcrypt.hashSync(password,10);
+    try {
+        const insertUser = db.prepare(`INSERT INTO users(username, password) VALUES(?,?)`);
+        const result = insertUser.run(username, hashedPassword)
+        
+        //Adding user's first todo
+        const defaultTodo = `Hello :) Add your first todo`;
+        const insertTodo = db.prepare(`INSERT INTO todos(user_id,task) VALUES(?,?)`);
+        insertTodo.run(result.lastInsertRowid, defaultTodo)
+
+        //create a token
+        const token =jwt.sign({id:result.lastInsertRowid},process.env.JWT_SECRET, {expiresIn:'24h'});
+        res.json({token});
+
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(503).json({message:error})
+    }
+    res.send({message:"User registered successfully"});
+    })
+
+//Login 
+router.post('/login', async(req, res)=>{
+   const {username,password} = req.body;
+   
+   try {
+      const getUser = db.prepare('SELECT * FROM users WHERE username =?')
+      const user = getUser.get(username)
+
+      //validate user and password
+      if(!user){return res.status(404).send({message:"user not found"})}
+
+      const passwordIsValid = bcrypt.compareSync(password, user.password)
+
+      if(!passwordIsValid){return res.status(401).send({message:"Invalid password"})}
+
+
+      //then we have a successfull authentication
+      const token = jwt.sign({id:user.id},process.env.JWT_SECRET,{expiresIn:'24h'})
+      res.json({token})
+      
+   } catch (error) {
+        console.log(error.message);
+        res.sendStatus(503)
+   }
+})
+
+
+export default router
